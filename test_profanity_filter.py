2fc5ef809c631d62ecf219f7c67a4b8a946b7da5
balerion7 <balerion7@users.nopley.github.com>
2016-01-04 21:33:48 -0600

    modified:   README.md 	modified:   profanity_filter.py 	new file:   test_profanity_filter.py

---
+++ test_profanity_filter.py
@@ -0,0 +1,62 @@
+from profanity_filter import Filter
+
+text = "Cassandra is a fuCking piece of shit_on_a_long_stick"
+
+print "We first test the three cleaning methods on the message:"
+print text
+print
+
+f=Filter(text, "HAPPY")
+print "Output from clean_anywhere:"
+print f.clean_anywhere()
+print "Output from clean_start:"
+print f.clean_start()
+print "Output from clean whole word:"
+print f.clean_whole_word()
+
+
+print
+
+text = "Cassy is an asset to our company."
+print "We now test the profanity check with the following:"
+print text
+print "Output from clean_anywhere:"
+
+f=Filter(text,'DADA')
+f.clean_anywhere()
+if not f.is_profanity_found():
+    print "No profanity."
+else:
+    print "Here is the clean anywhere:"
+    print f.get_clean_string()
+
+
+print
+#f=Filter(text,'DADA')
+print "Output from clean_start:"
+print f.clean_start()
+if not f.is_profanity_found():
+    print "No profanity."
+else:
+    print "Here is the clean string:"
+    print f.get_clean_string()
+
+print
+#f=Filter(text,'DADA')
+print "Output from clean whole word:"
+print f.clean_whole_word()
+if not f.is_profanity_found():
+    print "No profanity."
+else:
+    print "Here is the clean string:"
+    print f.get_clean_string()
+
+
+
+
+
+print "We now make sure the error handling is working."
+f = Filter(text, "SAD")
+#print f.is_profanity_found()
+print f.get_clean_string()
+
